1fc0e395d9961eb01ef8789c9b04c69c4e3e4b43
2264381489@qq.com <2264381489@qq.com>
2020-02-24 22:30:09 +0800

    645

--- src/Generic/Generic645/Customer.java
+++ src/Generic/Generic645/Customer.java
@@ -1,76 +1,67 @@
 package Generic.Generic645;
 
-import java.util.HashMap;
-import java.util.Map;
+import Generic.Generic_627_628_629_630.Generator;
+import Generic.Generic_635_636_637_638.Generators;
 
-//public class Customer {
-//}
-//    public int romanToInt(String s) {
-//        // 算法一：判断后面数值是否大于前面
-//        Map<Character, Integer> romaNumber = new HashMap<>();
-//        romaNumber.put('I', 1);
-//        romaNumber.put('V', 5);
-//        romaNumber.put('X', 10);
-//        romaNumber.put('L', 50);
-//        romaNumber.put('C', 100);
-//        romaNumber.put('D', 500);
-//        romaNumber.put('M', 1000);
-//
-//        int firstValue = 0;
-//        int nextValue = 0;
-//        int sum = 0;
-//
-//        for (int i = 0; i < s.length(); i++){
-//            firstValue = romaNumber.get(s.charAt(i));
-//            if (i == s.length()-1){
-//                sum += firstValue;
-//            }else {
-//                nextValue = romaNumber.get(s.charAt(i+1));
-//                if (firstValue >= nextValue){
-//                    sum += firstValue;
-//                }else{
-//                    sum -= firstValue;
-//                }
-//            }
-//        }
-//        return sum;
-//    }
-class Solution {
-    public int romanToInt(String s) {
-        Map<Character,Integer> hashmap=new HashMap();
-        hashmap.put('I',1);
-        hashmap.put('V',5);
-        hashmap.put('X',10);
-        hashmap.put('L',50);
-        hashmap.put('C',100);
-        hashmap.put('D',500);
-        hashmap.put('M',1000);
-        //应为在在左边的罗马数字要是小于右边的，就意味着这个是特殊情况。
-        //罗马数字中小的数字在大的数字的右边（！）
-        int firstvalue=0;
-        int secondvalue=0;
-        int total=0;
-        for(int i=0;s.length()>i;i++){
-            firstvalue=hashmap.get(s.charAt(i));
-            if(i== s.length()-1){
-                total+=hashmap.get(s.charAt(i));
-            }else{
-                secondvalue=hashmap.get(s.charAt(i+1));
-                if(firstvalue>=secondvalue){
-                    total+=hashmap.get(s.charAt(i));
-                }
-                else{
-                    total-=hashmap.get(s.charAt(i));
-                }
+import java.util.*;
+
+/**
+ * 645
+ * 匿名内部类，泛型还可以应用于内部类，以及匿名内部类。
+ * 引用的接口时自己写的。这个样子。
+ * public interface Generator<T> {
+ *     T next() throws IllegalAccessException, InstantiationException;
+ * }
+ *原文：
+ * 由于Customer和Tellar类都只有private的构造器。
+ *
+ */
+public class Customer {
+    private  static long counter=1;//静态变量，只有一个，也就是说没新建一个Customer对象，这个就增加一（结合下一个句子。）
+    private final long id=counter++;//计数的变量增加，赋值给常量。让常量发生变化。
+    private Customer(){ }//生成器必须要有缺省的
+    public String toString(){return "Customer"+id;}//toString方法，输出这个类的信息
+    public static Generator<Customer> generator(){//
+        return new Generator<Customer>() {
+            @Override
+            public Customer next() throws IllegalAccessException, InstantiationException {
+                return new Customer();//接口中必须实现的方法
             }
-
+            };//这里必须加入“;”因为这是内部匿名类。
         }
-        return total;
     }
-
-    public static void main(String[] args) {
-        Solution solution = new Solution();
-        System.out.println(solution.romanToInt("III"));
-
-    }
+    class Teller{
+        private  static long counter=1;
+        private final long id=counter++;
+        public Teller() { }
+        public String toString(){return "Teller"+id;}
+      public static Generator<Teller> generator(){
+            return new Generator<Teller>() {
+                @Override
+                public Teller next() throws IllegalAccessException, InstantiationException {
+                    return new Teller();
+                }
+            };
+      }
 }
+ class BamkTellar{
+    public static void serve(Teller a,Customer b){
+        System.out.println(a+"servies"+b);
+    }
+
+     public static void main(String[] args) throws IllegalAccessException, InstantiationException {
+         Random rand=new Random(47);
+         Queue<Customer> link=new LinkedList<Customer>();
+         // Generator是生成器对象，
+         Generators.filld(link,Customer.generator(),5);
+         List<Teller> tellers=new ArrayList<>();
+         Generators.filld(tellers,Teller.generator(),5);
+         //之所以使用这个方法，在于foreach句子不可能同时输入两个不同的容器。
+         //LIST和QUEUE都支持迭代器。
+         for (Customer c:link){
+             //rand用nextint方法的参数来控制出现的随机数的范围。
+             serve(tellers.get(rand.nextInt(tellers.size())), c);
+         }
+     }
+        }
+
--- src/Generic/Generic_627_628_629_630/Generator.java
+++ src/Generic/Generic_627_628_629_630/Generator.java
@@ -1,7 +1,8 @@
 package Generic.Generic_627_628_629_630;
 
 /**
- * 一个借口类
+ * 一个接口类，使用了泛型。可以看到他next方法的返回的类型被直接规定成了T。
+ * 这个是生成器接口
  * @param <T>
  */
 public interface Generator<T> {
