19bcdbbcd7cfcb229bd378789cdb6e373187743c
2264381489 <2264381489@qq.com>
2020-05-04 23:20:10 +0800

    容器更新

--- src/homework.java
+++ src/homework.java
@@ -1,4 +1,6 @@
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 
 public class homework {
 
@@ -37,5 +39,28 @@ class login{
     HashMap<String,String> hashMap=new HashMap<>();
     login(){
         hashMap.put(id,password);
+    }
+}
+class Solution {
+    public String addStrings(String num1, String num2) {
+        int i=0;
+        boolean flag=false;
+        List<Character> list=new LinkedList<>();
+        //任何一个字符串结束就停止
+        while(num1.length()>=i&&num2.length()>=i){
+            int num=num1.charAt(i)+num2.charAt(i);
+            if(flag){
+                num+=1;
+                flag=false;
+            }
+            if(num>10)flag=true;
+            list.add(num%10);
+            i++;
+        }
+        String str=list.toArray(String);
+        //如果长度不一样
+        return    num1.length()>num2.length()?str+num1.substring(i,num1.length()-1):str+num2.substring(i,num2.length()-1);
+
+
     }
 }
